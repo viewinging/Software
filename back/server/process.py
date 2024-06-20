031204ff469cf07b4927962f7cd046916bd1fd2a
hyeon seo <skybluehs0718@gmail.com>
2024-06-20 20:34:22 +0900

    server

---
+++ back/server/process.py
@@ -0,0 +1,5 @@
+# @app.route('/api/data', methods=['GET'])
+# def get_data():
+#     print("Received GET request for /api/data")
+#     # 처리 로직...
+#     return jsonify({'message': 'Data received'})
