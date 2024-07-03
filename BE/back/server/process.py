c754852ec446d46a4f243346f7b5c0dc985181e7
hyeon seo <skybluehs0718@gmail.com>
2024-07-04 08:02:10 +0900

    server.py

--- BE/back/server/process.py
+++
@@ -1,5 +0,0 @@
-# @app.route('/api/data', methods=['GET'])
-# def get_data():
-#     print("Received GET request for /api/data")
-#     # 처리 로직...
-#     return jsonify({'message': 'Data received'})
--- BE/back/server/server.py
+++ BE/back/server/server.py
@@ -7,4 +7,4 @@ def users():
     return 'hello world'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # host='0.0.0.0'으로 설정
+    app.run(host='0.0.0.0', port=5000)  # host='0.0.0.0'으로 설정.
