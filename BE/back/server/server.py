d578504c331f058d00869c538acb2f81fb9abaaf
hyeon seo <skybluehs0718@gmail.com>
2024-07-04 20:02:25 +0900

    server

--- BE/back/server/server.py
+++ BE/back/server/server.py
@@ -1,34 +1,34 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy #mariaDB
+from flask_cors import CORS, cross_origin   
 import random
 import string
 
 app = Flask(__name__)
-CORS(app) #모든 원본(cors)에서의 요청을 허용
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
-#데이터 베이스 설정
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/viewinging'
-db = SQLAlchemy(app)
+#DB설정
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/viewinging' #DB URI
+db = SQLAlchemy(app)    
 
-#데이터베이스 모델
+#DB모델
 class Nickname(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50), unique = True)
+    id = db.Column(db.Integer, primary_key = True) #ID coulmn(primary key)
+    name = db.Column(db.String(50), unique = True)  #NAME coulmn
 
     def __repr__(self):
         return f'<Nickname {self.name}>'
 
-@app.route("/")
-def get_nickname():
+@app.route("/") #기본경로
+def get_nickname(): #DB->nickname 함수
     nicknames = Nickname.query.all() #DB에서 모든 닉네임을 조회
-    nicknames_list = [{'id': nickname.id, 'name': nickname.name} 
+    nicknames_list = [{'id': nickname.id, 'name': nickname.name}
                     for nickname in nicknames]
-    return jsonify(nicknames_list)
+    return jsonify(nicknames_list)  #json형식-응답 변환
 
 
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() #DB 테이블 생성
-    app.run(host='0.0.0.0', port=5000)  # host='0.0.0.0'으로 설정.
+    app.run(host='0.0.0.0', port=5000)
