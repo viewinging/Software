1fc32ae021fe8a96b052273eb68f029c05f2687c
hyeon seo <skybluehs0718@gmail.com>
2024-07-04 17:30:32 +0900

    flask_server

--- BE/back/server/server.py
+++ BE/back/server/server.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import random
 import string
 
 app = Flask(__name__)
+CORS(app) #모든 원본(cors)에서의 요청을 허용
 
 #데이터 베이스 설정
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/viewinging'
 db = SQLAlchemy(app)
 
 #데이터베이스 모델
@@ -27,5 +29,6 @@ def get_nickname():
 
 
 if __name__ == '__main__':
-    db.create_all() #DB 테이블 생성
+    with app.app_context():
+        db.create_all() #DB 테이블 생성
     app.run(host='0.0.0.0', port=5000)  # host='0.0.0.0'으로 설정.
