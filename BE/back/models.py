e19172adf3aa0eb5b94ec9a80aca6bb369a0f50b
Hyeonseo071 <skybluehs0718@gmail.com>
2024-07-11 18:20:46 +0900

    DB && server manage

--- BE/back/models.py
+++ BE/back/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    userid = db.Column(db.String(32))
-    passward = db.Column(db.String(128))
+    userid = db.Column(db.String(32), unique= True, nullable = False)
+    passward = db.Column(db.String(128), nullable = False)
 
     
